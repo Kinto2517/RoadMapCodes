085520f8f529a1a4afd029fd1578b8953ef3f0e9
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-05-27 20:30:45 +0300

    root

---
+++ JAVA Projects/crudThyme1/src/test/java/com/kintothyme1/crudThyme1/CrudThyme1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kintothyme1.crudThyme1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudThyme1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
