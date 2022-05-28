9445947e23ee4a942a4b8c7915a0ba066b69b82f
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-05-29 01:35:46 +0300

    root

---
+++ JAVA Projects/logreg/src/main/java/com/example/logreg/LogregApplication.java
@@ -0,0 +1,13 @@
+package com.example.logreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogregApplication.class, args);
+	}
+
+}
---
+++ JAVA Projects/logreg/src/test/java/com/example/logreg/LogregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
