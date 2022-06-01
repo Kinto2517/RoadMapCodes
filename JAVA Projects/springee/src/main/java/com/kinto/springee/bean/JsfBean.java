be985f069eff11848c6f9ff56e6f900251024579
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-01 04:12:06 +0300

    root

---
+++ JAVA Projects/springee/src/main/java/com/kinto/springee/bean/JsfBean.java
@@ -0,0 +1,19 @@
+package com.kinto.springee.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named(value="cdiBean")
+@ApplicationScoped
+@Setter
+@Getter
+public class JsfBean {
+    private String patika;
+
+    public JsfBean() {
+        this.patika = "Spring Egitimi";
+    }
+}
---
+++ JAVA Projects/springee/src/main/java/com/kinto/springee/tutorials/JavaTutorials.java
@@ -0,0 +1,19 @@
+package com.kinto.springee.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+
+    public static void main(String[] args) {
+        //Arrays.sort();
+        //this.
+        //super()
+        //cast
+        //final
+        //static
+
+
+
+
+    }
+}
