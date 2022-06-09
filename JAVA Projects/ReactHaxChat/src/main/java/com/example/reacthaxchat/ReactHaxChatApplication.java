a43bee972c2e991c07ef222ef5b1d4640c9824e3
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-09 19:40:23 +0300

    Determined

---
+++ JAVA Projects/ReactHaxChat/src/main/java/com/example/reacthaxchat/ReactHaxChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.reacthaxchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactHaxChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactHaxChatApplication.class, args);
+    }
+
+}
---
+++ JAVA Projects/ReactHaxChat/src/test/java/com/example/reacthaxchat/ReactHaxChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reacthaxchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactHaxChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
