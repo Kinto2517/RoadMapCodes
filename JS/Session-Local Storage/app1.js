2326e4887481f94a82f4b8dd684d1ba6ea3cfd74
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-15 18:02:46 +0300

    root

---
+++ JS/Session-Local Storage/app1.js
@@ -0,0 +1,8 @@
+// localStorage.clear;
+
+// localStorage.setItem("har","burg");
+
+// console.log(localStorage.getItem("sport"));
+
+// localStorage.setItem("todos", JSON.stringify(todos));
+//const value = JSON.parse(localStorage.getItem("todos"));
