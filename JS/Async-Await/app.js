256a440d411078fae06b0e136f0bb5f822ef504e
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-17 13:17:34 +0300

    Async-Await

--- JS/Async-Await/app.js
+++ JS/Async-Await/app.js
@@ -1,5 +1,78 @@
-function test(data) {
-  return data;
+// async function test(data) {
+//   //   return data;
+
+//   let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Değer");
+//     }, 3000);
+//   });
+
+//   let response = await promise;
+
+//   console.log(response);
+//   console.log("naber");
+// }
+
+// async function getCurrency(url) {
+//   const response = await fetch(url);
+
+//   const data = await response.json();
+
+//   console.log(data);
+// }
+
+class Request {
+  async get(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  }
+
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      header: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    
+    const data = await response.json();
+
+    return data;
+
 }
 
-console.log(test("merhaba"));
+
+async put(url,data){
+    const response = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        header: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      
+      const data = await response.json();
+  
+      return data;
+  
+
+}
+async delete(url){
+    const response = await fetch(url, {
+        method: "DELETE"
+      });
+      
+      const data = await response.json();
+  
+      return "Başarılı Sİlindi";
+  
+
+
+}
+
+// getCurrency("jsonexchangeurl");
+// test("Hey");
+// test("mer").then((response) => console.log(response));
