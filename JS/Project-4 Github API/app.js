4874197a30f4564e64bc3dd5b48d6008a09809b3
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-21 13:28:06 +0300

    Github API Project

---
+++ JS/Project-4 Github API/app.js
@@ -0,0 +1,69 @@
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+
+
+const github = new Github();
+
+const ui = new UI();
+
+eventListeners();
+
+function eventListeners() {
+  githubForm.addEventListener("submit", getData);
+  clearLastUsers.addEventListener("click", clearAllSearched);
+  document.addEventListener("DOMContentLoaded", getAllSearched);
+}
+
+function getData(e) {
+  let username = nameInput.value.trim();
+
+  if (username === "") {
+    alert("kull");
+  } else {
+    github
+      .getGithubData(username)
+      .then((response) => {
+        if (response.user.message === "Not Found") {
+            ui.showError("Kullanıcı Bulunamadı");
+        } else {
+          console.log(response);
+
+          ui.addSearchedUserToUI(username);
+
+          Storage.addSearchedUserToStorage(username);
+          ui.showUserInfo(response.user);
+          ui.showRepoInfo(response.repo);
+        }
+      })
+      .catch((err) => ui.showError(err));
+  }
+  ui.clearInput();
+  e.preventDefault();
+}
+
+function clearAllSearched(){
+    if(confirm("Emin?")){
+        Storage.clearAllSearchedUsersFromStorage();
+        ui.clearAllSearchedFromUI();
+    }
+
+}
+
+
+function getAllSearched(){
+
+    let users = Storage.getSearchedUsersFromStorage();
+
+    let result = "";
+    users.forEach(user=>{
+
+        result += `<li class="list-group-item">${user}</li>`
+
+    })
+
+    lastUsers.innerHTML=result;
+}
+
+
