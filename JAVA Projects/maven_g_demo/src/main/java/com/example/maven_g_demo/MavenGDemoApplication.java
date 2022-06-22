a1d20dda1296d30b852c6dc8b22faf9ce6dd0523
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-22 12:09:27 +0300

    Maven _G_ Demo

---
+++ JAVA Projects/maven_g_demo/src/main/java/com/example/maven_g_demo/MavenGDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.maven_g_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenGDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenGDemoApplication.class, args);
+	}
+
+}
---
+++ JAVA Projects/maven_g_demo/src/test/java/com/example/maven_g_demo/MavenGDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_g_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenGDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
