c62aa959fd9eddcee51f5e71afa1bb377fbfc91e
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-07-03 11:53:30 +0300

    Updated project with logger.

---
+++ PatikaDevSpringProjects/HumanResourcesProject/src/main/java/com/example/humanresourcesproject/service/imp/UserServiceImp.java
@@ -0,0 +1,62 @@
+package com.example.humanresourcesproject.service.imp;
+
+import com.example.humanresourcesproject.model.User;
+import com.example.humanresourcesproject.repository.UserRepository;
+import com.example.humanresourcesproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    @Lazy
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @Override
+    public User save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRole("ROLE_USER");
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),  user.getAuthorities());
+
+    }
+
+
+}
