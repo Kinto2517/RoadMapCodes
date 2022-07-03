93fb8027ba3d89a1cd47214ea243e038def75fcb
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-07-03 09:11:37 +0300

    Started Human Resources Project. Will be add logging and paging

---
+++ PatikaDevSpringProjects/HumanResourcesProject/src/main/java/com/example/humanresourcesproject/HumanResourcesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.humanresourcesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourcesProjectApplication.class, args);
+    }
+
+}
---
+++ PatikaDevSpringProjects/HumanResourcesProject/src/test/java/com/example/humanresourcesproject/HumanResourcesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humanresourcesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
