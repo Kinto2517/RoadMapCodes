97e9b9a427b50b2825ad93d4bcafa618db132c47
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-07-17 23:44:15 +0300

    Initialized Spring for InstaDemo Backend

---
+++ ReactandSpring/spring3-insta/src/main/java/com/example/spring3insta/Spring3InstaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring3insta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3InstaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3InstaApplication.class, args);
+    }
+
+}
